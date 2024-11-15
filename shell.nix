c34f0cfeece4c41c59c465214e16c8a2e14cc60f
Gjergj Lala <g.lala@lalatech.de>
2024-11-15 16:42:23 +0100

    add nix-shell

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  unstable = import (fetchTarball "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz") {
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+        "terraform"
+      ];
+    };
+  };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    unstable.terraform
+  ];
+
+  shellHook = ''
+  '';
+}
