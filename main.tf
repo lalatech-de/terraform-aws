da7b002d9cc4610796c55fc35a2e0eab07e64fa5
Gjergj Lala <g.lala@lalatech.de>
2024-11-16 13:13:32 +0100

    add virtual private cloud

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "Terraform blog"
   }
@@ -38,4 +40,20 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
+module "ex_vpc" { # Virtual Private Cloud
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
