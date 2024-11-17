71911069620cd27ef41b1971c1f7654e7cc35f82
Gjergj Lala <g.lala@lalatech.de>
2024-11-17 16:25:02 +0100

    abstract variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,33 +11,34 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"]
+  owners = [var.ami_filter.owner]
 }
+
 
 module "ex_vpc" { # Virtual Private Cloud
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name 
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.0.0"
-  
+
   name = "blog"
 
-  min_size            = 1
-  max_size            = 2
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.ex_vpc.public_subnets
   # target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
@@ -52,17 +53,17 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.ex_vpc.vpc_id
-  subnets            = module.ex_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id          = module.ex_vpc.vpc_id
+  subnets         = module.ex_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
     blog-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = "i-0f6d38a07d50d080f"
+      name_prefix = "blog-"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = "i-0f6d38a07d50d080f"
     }
   }
 
@@ -77,7 +78,7 @@ module "blog_sg" {
 
   vpc_id              = module.ex_vpc.default_vpc_id
   name                = "blog"
-  ingress_rules       = ["https-443-tcp","http-80-tcp"]
+  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
